10bf842afc99f5df455d6b4109a4c9cdc89d9a69
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2025-06-02 13:57:03 +0200

    feat: implement slider message handling and add new actor classes for boids behavior

---
+++ src/main/java/pcd/ass03/actors/CohesionActor.java
@@ -0,0 +1,21 @@
+package pcd.ass03.actors;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+
+public abstract class CohesionActor extends AbstractActor {
+
+    public static Props props() {
+        return Props.create(CohesionActor.class);
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(SupervisorActor.CohesionMsg.class, msg -> {
+                    // TODO: Logica di coesione dei boids
+                })
+                .build();
+    }
+
+}
