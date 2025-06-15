6d2d739686b3159e197b5e9205995075f84d5cc5
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-06-15 12:50:56 +0200

    refactor: remove supervisor actor handling from BoidActor

--- src/main/java/pcd/ass03/actors/BoidActor.java
+++ src/main/java/pcd/ass03/actors/BoidActor.java
@@ -19,10 +19,7 @@ public class BoidActor extends AbstractActor {
     private final LoggingAdapter log;
     private V2d vel;
     private P2d pos;
-    private ActorRef nearbyActor;
-    private ActorRef supervisorActor;
-
-    public record SetSupervisorActorMsg(ActorRef supervisorActor) {}
+    private final ActorRef nearbyActor;
 
     /**
      * Message to request an update of the boid's state.
@@ -69,9 +66,6 @@ public class BoidActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .match(SetSupervisorActorMsg.class, msg -> {
-                    this.supervisorActor = msg.supervisorActor();
-                })
                 .match(UpdateRequestMsg.class, msg -> {
                     Boid current = new Boid(this.pos, this.vel);
                     nearbyActor.tell(new NearbyActor.calculateNeighborsMsg(current, msg.boids, msg.separation, msg.alignment, msg.cohesion,
@@ -81,7 +75,7 @@ public class BoidActor extends AbstractActor {
                     List<Boid> nearbyBoids = msg.boids;
                     update(nearbyBoids, msg.separation, msg.alignment, msg.cohesion, msg.avoidRadius, msg.maxSpeed,
                             msg.minX, msg.maxX, msg.minY, msg.maxY, msg.width, msg.height);
-                    supervisorActor.tell(new SupervisorActor.UpdatedBoidMsg(new Boid(this.pos, this.vel)), getSelf());
+                    getSender().tell(new SupervisorActor.UpdatedBoidMsg(new Boid(this.pos, this.vel)), getSelf());
                 })
                 .matchAny(msg -> log.info("Received unknown message: " + msg))
                 .build();
