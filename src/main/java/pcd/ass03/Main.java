0dbc6ad40ba2803d39b8f384a3f8b21cc8e8df05
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-06-28 10:58:05 +0200

    refactor: simplify ActorSystem creation

--- src/main/java/pcd/ass03/Main.java
+++ src/main/java/pcd/ass03/Main.java
@@ -2,16 +2,12 @@ package pcd.ass03;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
 
 import pcd.ass03.actors.SupervisorActor;
 import pcd.ass03.actors.TimerActor;
 import pcd.ass03.actors.ViewActor;
 import pcd.ass03.view.View;
 import pcd.ass03.view.ViewImpl;
-
-import java.io.File;
 
 /**
  * Entry point for the Boids simulation application.
@@ -22,8 +18,7 @@ public class Main {
 
     public static void main(final String[] args) {
         final View view = new ViewImpl();
-        final Config config = ConfigFactory.parseFile(new File("src/main/java/pcd/ass03/application.conf"));
-        final ActorSystem system = ActorSystem.create("BoidsSimulationSystem", config);
+        final ActorSystem system = ActorSystem.create("BoidsSimulationSystem");
         final ActorRef viewActor = system.actorOf(ViewActor.props(view), "view");
         final ActorRef timerActor = system.actorOf(TimerActor.props(), "timer");
         final ActorRef supervisorActor = system.actorOf(SupervisorActor.props(viewActor, timerActor), "supervisor");
